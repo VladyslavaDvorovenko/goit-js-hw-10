960f2c6daac4d6ccf924a2d24cd650b20892c5e1
VladyslavaDvorovenko <chempion0308@gmail.com>
2023-03-13 22:57:09 +0200

    .

--- src/index.js
+++ src/index.js
@@ -38,8 +38,8 @@ function onInput() {
       switch (err.message) {
         case '404': {
           Notiflix.Notify.failure('Oops, there is no country with that name');
-          countryInfo.innerHTML = '';
-          countryList.innerHTML = '';
+          countryInfoEl.innerHTML = '';
+          countryListEl.innerHTML = '';
           break;
         }
       }
